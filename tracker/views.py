da4dfac72cc6728a93b38e4328c878c85f81d069
Firstname  Surname <2313564m@student.gla.ac.uk>
2021-11-18 16:08:33 +0000

    Added login page

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+
+                return redirect(reverse('dashboard'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            messages.warning(request, 'Incorrect Details, Try Again!')
+            return redirect(reverse('login'))
+    else:
+        return render(request, 'login.html')
+
+
+def dashboard(request):
+    context_dict = {}
+
+    return render(request, 'dashboard.html', context=context_dict)
